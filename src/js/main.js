3cb8033a6f97a32e86c00c6004ab7ed89965b17e
unknown <boris.galac@gmail.com>
2024-04-25 13:59:14 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+///// HAM BTN AND NAV OPEN/CLOSEDS
+
+const hamBtn = document.querySelector(".ham-btn");
+const nav = document.querySelector(".nav");
+hamBtn.addEventListener("click", (e) => {
+  if (nav.getAttribute("aria-expanded") === "false") {
+    nav.setAttribute("aria-expanded", "true");
+    hamBtn.setAttribute("data-active", "true");
+  } else {
+    nav.setAttribute("aria-expanded", "false");
+    hamBtn.setAttribute("data-active", "false");
+  }
+});
